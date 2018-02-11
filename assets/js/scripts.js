e13f62bc3415909814338374b43fff7fc9663864
nancyhenry89 <nancyhenry89@gmail.com>
2018-02-11 19:37:21 +0200

    Applying comments

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,17 @@
 $(function() {
+    var isMobile = window.matchMedia("only screen and (max-width: 1024px)");
     $('a[href*=#]').on('click', function(e) {
         e.preventDefault();
+        if (isMobile.matches) {
+          $('html, body').animate({
+              scrollTop: $($(this).attr('href')).offset().top-60
+          }, 500, 'linear');
+        }else{
+
         $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
+            scrollTop: $($(this).attr('href')).offset().top-93
         }, 500, 'linear');
+      }
     });
+
 });
